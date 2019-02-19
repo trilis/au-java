fad2cec1aad689808fa0e6a7249c9347557a4463
Alexey Trilis <aatrilis2000@gmail.com>
2019-02-19 15:47:26 +0300

    Fix junit imports

--- hw1/src/test/java/me/trilis/au/java/hw1/HashTableTest.java
+++ hw1/src/test/java/me/trilis/au/java/hw1/HashTableTest.java
@@ -1,8 +1,8 @@
 package me.trilis.au.java.hw1;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HashTableTest {
 
--- hw1/src/test/java/me/trilis/au/java/hw1/LinkedListTest.java
+++ hw1/src/test/java/me/trilis/au/java/hw1/LinkedListTest.java
@@ -2,8 +2,6 @@ package me.trilis.au.java.hw1;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest {
